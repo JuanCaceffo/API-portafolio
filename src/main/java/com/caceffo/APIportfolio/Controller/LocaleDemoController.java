20727104145fc359193a9992a275133ff28fddf6
JuanCaceffo <caceffojuan@gmail.com>
2024-02-13 22:30:13 -0300

    agregado de endpoint para controller de prueba de internacionalizacion

--- src/main/java/com/caceffo/APIportfolio/Controller/LocaleDemoController.java
+++ src/main/java/com/caceffo/APIportfolio/Controller/LocaleDemoController.java
@@ -1,8 +1,12 @@
 package com.caceffo.APIportfolio.Controller;
 
 import org.springframework.context.MessageSource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Locale;
 
 @RestController
 @RequestMapping("locale")
@@ -11,6 +15,11 @@ public class LocaleDemoController {
 
     public LocaleDemoController(MessageSource messageSource){
         this.messageSource = messageSource;
+    }
+
+    @GetMapping("/hello")
+    public String hellowWorld(@RequestHeader(name = "Accept-Language", required = false) Locale locale){
+        return messageSource.getMessage("greeting",null,locale);
     }
 
 
