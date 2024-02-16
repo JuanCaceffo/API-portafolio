5ea1d2d02cf1f2afe5c6ef22e098ae2f717c7cb9
JuanCaceffo <caceffojuan@gmail.com>
2024-02-16 12:42:22 -0300

    Already domain for multilanguague

--- src/main/java/com/caceffo/APIportfolio/Domain/WorkExperiencie.java
+++ src/main/java/com/caceffo/APIportfolio/Domain/WorkExperiencie.java
@@ -1,5 +1,6 @@
 package com.caceffo.APIportfolio.Domain;
 
+import com.caceffo.APIportfolio.Domain.helpers.Langs;
 import com.caceffo.APIportfolio.Errors.BusinessException;
 import com.caceffo.APIportfolio.Reposiroty.RepositoryProps;
 import org.joda.time.Months;
@@ -7,12 +8,12 @@ import org.joda.time.LocalDate;
 import java.util.Optional;
 
 public class WorkExperiencie extends RepositoryProps {
-    public String title;
-    public String description;
+    public Langs title;
+    public Langs description;
     public LocalDate startDate;
     public LocalDate finishDate;
 
-    public WorkExperiencie(String title, String description, LocalDate startDate, LocalDate finishDate){
+    public WorkExperiencie(Langs title, Langs description, LocalDate startDate, LocalDate finishDate){
         this.title = title;
         this.description = description;
         this.startDate = startDate;
--- src/test/java/com/caceffo/APIportfolio/domain/WorkExperiencieSpec.java
+++ src/test/java/com/caceffo/APIportfolio/domain/WorkExperiencieSpec.java
@@ -1,6 +1,7 @@
 package com.caceffo.APIportfolio.domain;
 
 import com.caceffo.APIportfolio.Domain.WorkExperiencie;
+import com.caceffo.APIportfolio.Domain.helpers.Langs;
 import com.caceffo.APIportfolio.Errors.BusinessException;
 import org.joda.time.LocalDate;
 import org.junit.jupiter.api.AfterEach;
@@ -28,7 +29,7 @@ public class WorkExperiencieSpec {
 
     @Test
     void the_total_months_of_a_worckExperienice_with_valid_start_and_finish_dates_worcks_fine() {
-        WorkExperiencie experiencie = new WorkExperiencie("hds", "data", new LocalDate(2024, 01, 01), new LocalDate(2024, 02, 01));
+        WorkExperiencie experiencie = new WorkExperiencie(new Langs("lala","lala"), new Langs("lala","lala"), new LocalDate(2024, 01, 01), new LocalDate(2024, 02, 01));
         assertEquals(experiencie.totalMonths(), 1);
     }
 
@@ -36,7 +37,7 @@ public class WorkExperiencieSpec {
     void worckExperiencie_without_finishDate_is_remplazed_with_actual_date(){
         LocalDate currentDate = new LocalDate(2024,02,02);
         localDateMockedStatic.when(LocalDate::now).thenReturn(currentDate);
-        WorkExperiencie experiencie = new WorkExperiencie("hds", "data", new LocalDate(2024, 01, 01),null);
+        WorkExperiencie experiencie = new WorkExperiencie(new Langs("lala","lala"), new Langs("lala","lala"), new LocalDate(2024, 01, 01),null);
         //Assert
         assertEquals(experiencie.finishDate,currentDate);
     }
@@ -45,7 +46,7 @@ public class WorkExperiencieSpec {
     void startDate_of_the_worckExperiencie_is_greater_than_the_finshDate(){
         //Arragne
         Exception exception = assertThrows(BusinessException.class, () -> {
-            new WorkExperiencie("hds", "data", new LocalDate(2024, 03, 01), new LocalDate(2024, 02, 01));
+            new WorkExperiencie(new Langs("lala","lala"), new Langs("lala","lala"), new LocalDate(2024, 03, 01), new LocalDate(2024, 02, 01));
         });
         //Assert
         assertTrue(exception.getMessage().contains("Exception.WorckExperiencie.invalidStartDate"));
