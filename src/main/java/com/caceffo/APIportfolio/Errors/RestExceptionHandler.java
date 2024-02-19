c2df2efa55e1f61a7d609918fa64d11139b60858
JuanCaceffo <caceffojuan@gmail.com>
2024-02-19 18:48:46 -0300

    agregado de documentacion con OpenAPI y swagger

--- src/main/java/com/caceffo/APIportfolio/Errors/RestExceptionHandler.java
+++ src/main/java/com/caceffo/APIportfolio/Errors/RestExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.Locale;
 
@@ -21,12 +22,14 @@ public class RestExceptionHandler {
     }
 
     @ExceptionHandler(BusinessException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<MessageDTO> handleBussinesException(BusinessException ex, Locale locale){
         String message = messageSource.getMessage(ex.getMessage(),ex.params,locale);
         return new ResponseEntity<>(new MessageDTO(message) ,HttpStatus.BAD_REQUEST);
     }
 
     @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<MessageDTO> handleExceptions(Exception ex, Locale locale) {
         String errorMessage = messageSource.getMessage(UNEXPECTED_ERROR, null, locale);
         ex.printStackTrace();
